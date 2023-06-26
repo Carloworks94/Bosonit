3f2a19a06e9d31d43dca05ebbccb6fe5117a225e
carlos.garrido <carlos.garrido@bosonit.com>
2023-06-26 12:39:18 +0200

    Correcion de errores bloque1

--- bloque1/src/main/java/com/bosonit/Main.java
+++ bloque1/src/main/java/com/bosonit/Main.java
@@ -19,7 +19,21 @@ import java.util.stream.Collectors;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    int numDatos = 3; // meter en una interfaz de constante
+    // FIXME: meter en una interfaz de constantes
+    public static int numDatos = 3;
+    public static String formatoIncorrecto = "Formato de linea incorrecto en la linea ";
+
+    public static void main(String[] args) throws IOException {
+        String ruta = "src/main/java/com/bosonit/utils/Personas.csv";
+
+        //EJERCICIO 1a
+        System.out.println("Apartado A)");
+        List<Person> personas = conversionCSV(ruta);
+        personas = filtraPersonas(personas);
+        for (Person persona : personas) {
+            System.out.println(persona.toString());
+        }
+    }
 
     private static List<Person> conversionCSV (String ruta) throws IOException{
         List<Person> personas = new ArrayList<>();
@@ -31,15 +45,15 @@ public class Main {
                 numLinea++;
                 String[] aux = linea.split(":", -1);  //-1 para contar el último campo aunque esté vacío
                 Person person = new Person(aux[0]);
-                if (aux.length==3) {
+                if (aux.length==numDatos) {
                     if (aux[0].length()<1)
-                        throw new InvalidLineFormatException("Formato de linea incorrecto en la linea " + numLinea);
+                        throw new InvalidLineFormatException(formatoIncorrecto + numLinea);
                     if (aux[1]!="")
                         person.setTown(aux[1]);
                     if (aux[2]!="")
                         person.setAge(Integer.parseInt(aux[2]));
                 }else{
-                    throw new InvalidLineFormatException("Formato de linea incorrecto en la linea " + numLinea);
+                    throw new InvalidLineFormatException(formatoIncorrecto + numLinea);
                 }
                 personas.add(person);
             }
@@ -49,7 +63,7 @@ public class Main {
         return personas;
     }
 
-   public static List<Person> filtraPersonas (List<Person> personas){
+   private static List<Person> filtraPersonas (List<Person> personas){
         List<Person> filtro = personas.stream()
                 .filter(person -> person.getAge()< 25)
                 .filter(person -> person.getAge()> 0)
@@ -57,16 +71,4 @@ public class Main {
         return filtro;
     }
 
-
-    public static void main(String[] args) throws IOException {
-        String ruta = "bloque1/src/main/java/com/bosonit/utils/Personas.csv";
-
-        //EJERCICIO 1a
-        System.out.println("Apartado A)");
-        List<Person> personas = conversionCSV(ruta);
-        personas = filtraPersonas(personas);
-        for (Person persona : personas) {
-            System.out.println(persona.toString());
-        }
-    }
 }
