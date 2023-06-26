7eacb288d28021dfeb5cbdac0e217187072a977a
carlos.garrido <carlos.garrido@bosonit.com>
2023-06-26 13:07:44 +0200

    Correcion de errores2 bloque1

--- bloque1/src/main/java/com/bosonit/Main.java
+++ bloque1/src/main/java/com/bosonit/Main.java
@@ -19,12 +19,12 @@ import java.util.stream.Collectors;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    // FIXME: meter en una interfaz de constantes
-    public static int numDatos = 3;
-    public static String formatoIncorrecto = "Formato de linea incorrecto en la linea ";
+
+    private static final int NUMDATOS = 3;
+    private static final String FORMATOINCORRECTO = "Formato de linea incorrecto en la linea ";
 
     public static void main(String[] args) throws IOException {
-        String ruta = "src/main/java/com/bosonit/utils/Personas.csv";
+        String ruta = "src/main/resources/Personas.csv";
 
         //EJERCICIO 1a
         System.out.println("Apartado A)");
@@ -45,15 +45,15 @@ public class Main {
                 numLinea++;
                 String[] aux = linea.split(":", -1);  //-1 para contar el último campo aunque esté vacío
                 Person person = new Person(aux[0]);
-                if (aux.length==numDatos) {
+                if (aux.length==NUMDATOS) {
                     if (aux[0].length()<1)
-                        throw new InvalidLineFormatException(formatoIncorrecto + numLinea);
+                        throw new InvalidLineFormatException(FORMATOINCORRECTO + numLinea);
                     if (aux[1]!="")
                         person.setTown(aux[1]);
                     if (aux[2]!="")
                         person.setAge(Integer.parseInt(aux[2]));
                 }else{
-                    throw new InvalidLineFormatException(formatoIncorrecto + numLinea);
+                    throw new InvalidLineFormatException(FORMATOINCORRECTO + numLinea);
                 }
                 personas.add(person);
             }
